a9ea89bae9ee7d849e4add2c723994c7cbea0c27
VNavrockij <vitalik.nav@gmail.com>
2023-12-05 13:22:20 +0200

    Initial Commit

---
+++ TheRickAndMorty_CleanArch+MVVM+Coordinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheRickAndMorty_CleanArch+MVVM+Coordinator
+//
+//  Created by Vitalii Navrotskyi on 05.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
